8c7228ed15d8380d9b563b6714419d3cfe4cf0e7
Gustavo <gustavomatachun.domingues@gmail.com>
2025-06-02 15:12:12 -0300

    atualizando views

--- src/main/java/com/maislimpo/maislimpo/MaislimpoApplication.java
+++ src/main/java/com/maislimpo/maislimpo/MaislimpoApplication.java
@@ -1,11 +1,25 @@
 package com.maislimpo.maislimpo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.swing.SwingUtilities;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import com.maislimpo.maislimpo.view.TelaLogin;
 
 @SpringBootApplication
 public class MaislimpoApplication {
 
-	 public static void main(String[] args) {
-	
-	 }
+	public static void main(String[] args) {
+		// inicializando o spring pra ser compatível com swing
+		ConfigurableApplicationContext context = new SpringApplicationBuilder(MaislimpoApplication.class)
+				.headless(false)
+				.run(args);
+
+		// garante que a UI seja criada na Event Dispatch Thread (EDT)
+		SwingUtilities.invokeLater(() -> {
+			
+			TelaLogin telaLogin = context.getBean(TelaLogin.class); 
+			telaLogin.setVisible(true);
+		});
+	}
 }
